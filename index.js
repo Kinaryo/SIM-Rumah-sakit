923e7bbedf71154d53c5d7b980589f3b4a4361a8
Kinaryo <kinaryo733huda@gmail.com>
2023-12-19 05:30:20 +0700

    setup fronend

--- index.js
+++ index.js
@@ -27,15 +27,73 @@ app.use(express.static(path.join(__dirname,'public')))
 
 
 app.get('/', (req,res)=>{
-    res.render('pages/index')
+    res.render('admin/index')
 })
 
-app.get('/pages/kartuberobat',(req,res)=>{
-    res.render('pages/kartuberobat')
+
+//  admin
+app.get('/kartuberobat',(req,res)=>{
+    res.render('admin/pendaftaran/kartuberobat')
 })
-app.get('/pages/formulirpasien',(req,res)=>{
-    res.render('pages/formulirpasien')
+app.get('/formulirpasien',(req,res)=>{
+    res.render('admin/pendaftaran/formulirpasien')
 })
+app.get('/daftarpasien',(req,res)=>{
+    res.render('admin/daftarpasien')
+})
+app.get('/daftarkunjungan',(req,res)=>{
+    res.render('admin/daftarkunjungan')
+})
+app.get('/asuransi',(req,res)=>{
+    res.render('admin/asuransi')
+})
+// laporan
+app.get('/laporan/harian',(req,res)=>{
+    res.render('rawatInap/laporan/harian')
+})
+app.get('/laporan/mingguan',(req,res)=>{
+    res.render('rawatInap/laporan/mingguan')
+})
+app.get('/laporan/bulanan',(req,res)=>{
+    res.render('rawatInap/laporan/bulanan')
+})
+
+
+// app.get('',(req,res)=>{
+//     res.render('admin/daftarpasien')
+// })
+
+
+// Perawat
+app.get('/rawatinap/pasien',(req,res)=>{
+    res.render('rawatInap/daftarPasien')
+})
+//laporan perawat
+app.get('/rawatinap/laporan/harian',(req,res)=>{
+    res.render('rawatInap/laporan/harian')
+})
+app.get('/rawatinap/laporan/mingguan',(req,res)=>{
+    res.render('rawatInap/laporan/mingguan')
+})
+app.get('/rawatinap/laporan/bulanan',(req,res)=>{
+    res.render('rawatInap/laporan/bulanan')
+})
+//farmasi & logistik
+app.get('/rawatinap/farmasilogistik/harian',(req,res)=>{
+    res.render('rawatInap/farmasiLogistik/harian')
+})
+app.get('/rawatinap/farmasilogistik/mingguan',(req,res)=>{
+    res.render('rawatInap/farmasiLogistik/mingguan')
+})
+app.get('/rawatinap/farmasilogistik/bulanan',(req,res)=>{
+    res.render('rawatInap/farmasiLogistik/bulanan')
+})
+// data kamar dan permintaan makan
+app.get('/rawatinap/datakamar', (req,res)=>{
+    res.render('rawatinap/dataKamar')
+})
+
+
 app.listen(PORT,()=>{
     console.log(`Server is running on http://127.0.0.1:${PORT}`)
 })
